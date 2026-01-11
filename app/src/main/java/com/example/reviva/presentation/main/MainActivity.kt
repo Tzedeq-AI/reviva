0cc45746a80aa2e1c5d256b752d06eda4c4fe32c
sam-shervin <samshervin.sebastian@gmail.com>
2026-01-12 01:24:12 +0530

    chore: establish clean architecture package structure

--- app/src/main/java/com/example/reviva/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/reviva/presentation/main/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.reviva.R
 
 class MainActivity : AppCompatActivity() {
 
