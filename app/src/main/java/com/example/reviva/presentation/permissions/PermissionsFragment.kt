93f38a401f6fb4495740e399e3d46705e5804b0f
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2026-01-12 03:37:32 +0530

    Setup navigation graph and screen state architecture (#77)
    * Add pre-Android 12 splash fallback and fix launch background
    
    * chore: establish clean architecture package structure
    
    * Navigation/base graph (#76)
    
    * feat: setup navigation graph and base screen flow
    
    * Setup navigation graph and screen state architecture

---
+++ app/src/main/java/com/example/reviva/presentation/permissions/PermissionsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.reviva.presentation.permissions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.reviva.R
+
+class PermissionsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.frag_permissions, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.actionBtn).setOnClickListener {
+            findNavController().navigate(R.id.action_permissions_to_home)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/reviva/presentation/save/SaveOrganizeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.reviva.presentation.save
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
+import com.example.reviva.presentation.camera.state.ScanFlowViewModel
+import com.example.reviva.R
+
+
+class SaveOrganizeFragment : Fragment() {
+    private val scanViewModel: ScanFlowViewModel by navGraphViewModels(R.id.app_nav_graph)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.frag_saveorganize, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        scanViewModel.reset()
+
+        view.findViewById<View>(R.id.actionBtn).setOnClickListener {
+            findNavController().navigate(R.id.action_save_to_home)
+        }
+    }
+}
