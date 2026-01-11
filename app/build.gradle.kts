e3f024875061410df8ccd8db1e18f0506ddc7c5d
sam-shervin <samshervin.sebastian@gmail.com>
2026-01-12 02:58:16 +0530

    feat: setup navigation graph and base screen flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.core:core-splashscreen:1.2.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
 }
---
+++ app/src/main/java/com/example/reviva/presentation/about/AboutFragment.kt
@@ -0,0 +1,19 @@
+package com.example.reviva.presentation.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.reviva.R
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.frag_about, container, false)
+    }
+}
